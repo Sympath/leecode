df71a7d211daeb39097efa95745c914728ecbeb3
wzyan <3101885298@qq.com>
2020-06-02 17:24:21 +0800

    递归处理 子集

---
+++ js-leecode/78.子集.js
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=78 lang=javascript
+ *
+ * [78] 子集
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+    let ans = [];
+
+    let dfs = function (nums,index,list) {
+        // recursion terminator  终止条件
+            if(index == nums.length){
+                ans.push(list);
+                return;
+            }
+
+        // process logic in current level  处理当前层逻辑代码  
+        // drill down  进入下一层
+          
+            dfs(nums,index+1,[...list]);
+            list.push(nums[index])
+            dfs(nums,index + 1,[...list]);
+           
+        // reverse the current level status if needed  如果需要，清理当前层状态
+            // list.pop()
+    }
+    dfs(nums, 0, []);
+
+    return ans;
+};
+// @lc code=end
+
